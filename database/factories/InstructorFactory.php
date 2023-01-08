2197be7f9846ae8d0db62a2e2880e7203a84277a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-08 22:43:47 +0500

    Speciifes fields in factory

--- database/factories/InstructorFactory.php
+++ database/factories/InstructorFactory.php
@@ -11,10 +11,16 @@ use App\Models\Instructor;
 class InstructorFactory extends Factory
 {
     protected $model = Instructor::class;
-    
+
     public function definition()
     {
         return [
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'phone_number' => $this->faker->phoneNumber,
+            'email' => $this->faker->email,
+            'status' => $this->faker->randomElement(['Active', 'Inactive']),
+            'courses_count' => $this->faker->numberBetween(1, 4),
         ];
     }
 }

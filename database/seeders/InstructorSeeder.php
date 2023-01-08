79ba19fab48f76a8fb323508d5b6a1191de98762
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-08 22:53:42 +0500

    Adds code to create records via factory

--- database/seeders/InstructorSeeder.php
+++ database/seeders/InstructorSeeder.php
@@ -4,10 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Instructor;
 
 class InstructorSeeder extends Seeder
 {
     public function run(): void
     {
+        Instructor::factory()
+            ->count(10) 
+            ->create();
     }
 }

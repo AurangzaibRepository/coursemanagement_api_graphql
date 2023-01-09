f8f850d5502fe31a447be7c2adb6547bfe99030d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-09 15:25:13 +0500

    Amends fullanme accessor name

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -18,7 +18,7 @@ class Instructor extends Model
         'courses_count',
     ];
 
-    protected function getFullName(): Attribute
+    protected function fullName(): Attribute
     {
         return Attribute::make(
             get: fn($value, $attributes) => "{$attributes['first_name']} {$attributes['last_name']}",

2c9e1ceffa8d3776102663108d9a814e2d94727e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-09 14:47:18 +0500

    Inlcues packages and ndamespace

---
+++ app/GraphQL/Queries/Instructor/InstructorQuery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\GraphQL\Queries\Instructor;
+
+use App\Models\Instructor;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class InstructorQuery extends Query
+{
+}

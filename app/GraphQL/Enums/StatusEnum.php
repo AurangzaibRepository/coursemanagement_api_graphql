6a86cc96a3368d35bc83ea4668985ce3e9c957c0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-09 15:44:00 +0500

    Specifies attributes

--- app/GraphQL/Enums/StatusEnum.php
+++ app/GraphQL/Enums/StatusEnum.php
@@ -6,4 +6,12 @@ use Rebing\GraphQL\Support\EnumType;
 
 class StatusEnum extends EnumType
 {
+    protected $attributes = [
+        'name' => 'Status',
+        'description' => 'List of status',
+        'values' => [
+            'Active' => 'Active',
+            'Inactive' => 'Inactive',
+        ],
+    ];
 }

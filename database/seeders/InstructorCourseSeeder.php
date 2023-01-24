c73a251ba5bf7a21d6d697a6c7dcd1936fc705c4
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-24 16:10:19 +0500

    Creates seeder for instructor couse

---
+++ database/seeders/InstructorCourseSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class InstructorCourseSeeder extends Seeder
+{
+    public function run(): void
+    {
+    }
+}

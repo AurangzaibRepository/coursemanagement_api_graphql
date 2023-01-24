706a6204b32f701a8280b5384d8482229100f745
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-24 14:11:32 +0500

    Defines relationship with courses

--- app/Models/Instructor.php
+++ app/Models/Instructor.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,6 +18,11 @@ class Instructor extends Model
         'email',
         'courses_count',
     ];
+
+    public function courses(): Relation
+    {
+        return $this->belongsToMany(Course::class, 'instructor_courses');
+    }
 
     protected function fullName(): Attribute
     {

90c34c73660425d587623a467a971abacaf8cdd1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-24 16:05:34 +0500

    Includes model

--- database/factories/InstructorCourseFactory.php
+++ database/factories/InstructorCourseFactory.php
@@ -3,12 +3,17 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\InstructorCourse;
+use App\Models\Instructor;
+use App\Models\Course;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InstructorCourse>
  */
 class InstructorCourseFactory extends Factory
 {
+    protected $model = InstructorCourse::class;
+
     public function definition(): array
     {
         return [

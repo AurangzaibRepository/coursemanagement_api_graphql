05dec48d8032ddcf91b20ad7315cb89c245b92c0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-24 14:39:43 +0500

    Includes model

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Course;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
